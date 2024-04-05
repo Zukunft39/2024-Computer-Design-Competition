c71ed5e4615d50ee8fe052af5cfd2bb14d4b9e31
woshiyuanfangdeni <117359194+woshiyuanfangdeni@users.noreply.github.com>
2024-04-05 23:13:50 +0800

    最最后一次修改

--- Assets/Scripts/Abacus/AbacusManager.cs
+++ Assets/Scripts/Abacus/AbacusManager.cs
@@ -206,12 +206,24 @@ public class AbacusManager : MonoBehaviour
     IEnumerator Anim()
     {
         isPause = true;
-        animB.SetInteger("move", 1);
-        yield return new WaitForSeconds(1.2f);
-        animF.SetTrigger("tick");
-        yield return new WaitForSeconds(0.8f);
-        animB.SetInteger("move", 0);
-        if (animB.GetInteger("move") == 0)
+        if (isSatisfied())
+        {
+            animB.SetInteger("move", 1);
+            yield return new WaitForSeconds(1.2f);
+            animF.SetTrigger("tick");
+            yield return new WaitForSeconds(0.8f);
+            animB.SetInteger("move", 0);
+        }
+        else
+        {
+            animB.SetInteger("move_cross", 1);
+            yield return new WaitForSeconds(1.2f);
+            animF.SetTrigger("cross");
+            yield return new WaitForSeconds(0.8f);
+            animB.SetInteger("move_cross", 0);
+        }
+        isPause = false;
+        if (animB.GetInteger("move") == 0 || animB.GetInteger("move_cross") == 0)
         {
 
             if (isSatisfied()) count++;
